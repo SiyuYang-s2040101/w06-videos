850b9a3451dacbd3a8eeff8db8b38c15c12178bc
Siyu Yang <siyuyang@SiyudeMacBook-Pro.local>
2020-10-30 08:54:43 +0000

    enter

---
+++ vids_1_2/iterables_iterators.py
@@ -0,0 +1,11 @@
+# iterables and interators
+
+mylist = [1, 3, 5, 7]
+itr_mylist = iter(mylist)
+
+print(next(itr_mylist))
+print(next(itr_mylist))
+print(next(itr_mylist))
+print(next(itr_mylist))
+
+# next print(next(itr_mylist)), ERROR!!!
